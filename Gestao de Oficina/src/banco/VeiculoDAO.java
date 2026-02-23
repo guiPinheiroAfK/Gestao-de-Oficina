72794dcce7b729d1b5c4a19a9108ba3d0ddc88cd
Guilherme <guivalen00@gmail.com>
2026-02-23 16:54:20 -0300

    feat: implementa herança, DAO e integra persistência com Docker
    Configurada infraestrutura Maven e dependência JDBC do PostgreSQL.
    
    Implementada hierarquia de classes (Veiculo, Carro, Moto) com abstração.
    
    Criado VeiculoDAO com métodos de salvar e buscar (CRUD).
    
    Corrigidos conflitos de SDK e Classpath do ambiente de desenvolvimento.

--- Gestao de Oficina/src/banco/VeiculoDAO.java
+++ Gestao de Oficina/src/banco/VeiculoDAO.java
@@ -1,9 +1,14 @@
 package banco;
 
 import modelo.Veiculo;
+import modelo.Carro;
+import modelo.Moto;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VeiculoDAO {
 
@@ -26,4 +31,32 @@ public class VeiculoDAO {
             throw new RuntimeException("Erro ao salvar veiculo: " + e.getMessage());
         }
     }
+
+    public List<Veiculo> buscarTodos() {
+        String sql = "SELECT * FROM veiculos";
+        List<Veiculo> veiculosL = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                String tipo = rs.getString("tipo");
+                String placa = rs.getString("placa");
+                String modelo = rs.getString("modelo");
+                int ano = rs.getInt("ano");
+
+                // lógica para instanciar a classe correta
+                if (tipo.equalsIgnoreCase("carro")) {
+                    veiculosL.add(new Carro(placa, modelo, ano));
+                } else
+                    veiculosL.add(new Moto(placa, modelo, ano));
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException("Erro ao listar veiculos: " + e.getMessage());
+            }
+        return veiculosL;
+    }
+
 }
